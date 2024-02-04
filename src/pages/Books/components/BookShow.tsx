9998270155b612b84791ea5f9fd7a5d7a7a1def2
alchogh <alchogh01@gmail.com>
2024-02-04 19:10:01 +0900

    🐛 삭제 버튼 함수 수정

--- src/pages/Books/components/BookShow.tsx
+++ src/pages/Books/components/BookShow.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Book } from "../Book";
+import { BookEdit } from "./BookEdit";
 import { styled } from "styled-components";
 interface BookShowProps {
   books: Book[];
@@ -7,15 +9,33 @@ interface BookShowProps {
 }
 
 export const BookShow = ({ book, setBooks, books }: BookShowProps) => {
-  const deleteList = () => {
-    const filterList = books.filter((v: any) => v.title !== book.title);
+  const [showEdit, setShowEdit] = useState(false);
+  const deleteList = (id: number) => {
+    const filterList = books.filter((v: Book) => v.id !== id);
     return setBooks(filterList);
   };
 
+  const handleDeleteClick = () => {
+    deleteList(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit />;
+  }
+
+  console.log(showEdit);
   return (
     <Container>
-      <p>{book.title}</p>
-      <DeleteButton onClick={deleteList}>삭제</DeleteButton>
+      <div>{content}</div>
+      <ButtonBox>
+        <EditButton onClick={handleEditClick}>수정</EditButton>
+        <DeleteButton onClick={handleDeleteClick}>삭제</DeleteButton>
+      </ButtonBox>
     </Container>
   );
 };
@@ -27,6 +47,30 @@ const Container = styled.div`
   padding: 10px;
   background-color: orange;
   margin-top: 10px;
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 150px;
+`;
+
+const EditButton = styled.button`
+  border: none;
+  padding: 10px;
+  width: 70px;
+  height: 40px;
+  border-radius: 6px;
+  background-color: gray;
+  color: white;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+  &:hover {
+    background-color: black;
+    font-weight: 700;
+    transition: 0.5s;
+  }
 `;
 
 const DeleteButton = styled.button`
